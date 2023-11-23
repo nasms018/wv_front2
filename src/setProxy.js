88f0e2c7e0924f58e99ef0fcc6c2f2f5c398423c
nasms018 <cjm1s@naver.com>
2023-11-23 21:03:02 +0900

    2

--- src/setProxy.js
+++ src/setProxy.js
@@ -3,7 +3,7 @@ import {createProxyMiddleware} from "http-proxy-middleware";
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/", {
-      target: "http://ec2-52-78-208-249.ap-northeast-2.compute.amazonaws.com:8080/",
+      target: "http://52.78.208.249:8080/",
       changeOrigin: true,
     })
   );
